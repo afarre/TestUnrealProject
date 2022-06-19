8d247d27aa33c9ed2495015bcb49cf303307c64c
usuario <usuario@DESKTOP-TALIUS>
2022-06-19 14:04:35 +0200

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/TestProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30145
+#include "C:/Users/usuario/OneDrive/Documents/Unreal Projects/TestProject/Intermediate/Build/Win64/TestProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
